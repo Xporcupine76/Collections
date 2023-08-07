88ffd8e428bbd5b4b21ba53c936d1c4ef2f93168
STUDIO 15 <STUDIO 15@DESKTOP-HFOS1SF>
2023-08-07 09:01:55 +0200

    Add project files.

---
+++ Collections/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+namespace Collections
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            //creating Collections and Array
+            var dogs = new List<string>();
+            dogs.Add("Bulldog");
+            dogs.Add("Collie");
+            dogs.Add("German Shepherd");
+            dogs.Add("Chiuaua");
+            dogs.Add("Greyhound");
+            dogs.Add("Pit bull");
+
+            dogs.Remove("Collie");
+            dogs.Add("German Shepherd");
+
+            //foreach to move throughout the list of dogs
+            foreach (var dog in dogs)
+                Console.WriteLine(dog+ "");
+            Console.ReadLine();
+
+            dogs.Remove("Collie");
+           
+
+
+
+
+
+
+
+
+
+
+        }
+
+
+
+    }
+
+
+
+}
